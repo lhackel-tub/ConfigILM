15e12e0bd09eb27ccbaf992fa48b7c6edd0f2f02
Leonard Hackel <l.hackel@tu-berlin.de>
2023-02-01 16:55:30 +0100

    adding mock csv and changing mock data

--- configvlm/extra/mock_data/gen_mock_data.py
+++ configvlm/extra/mock_data/gen_mock_data.py
@@ -1,34 +1,54 @@
+import csv
 import json
+import pathlib
+import random
 from os.path import join
-from typing import Collection
+from typing import Sequence
 
 import lmdb
 
 
-def copy_part_of_json(base_path: str, f_name: str, elems: int = 25):
+def copy_part_of_json(
+    base_path: str, f_name: str, elems: int = 25, random_seed: int = 0
+):
+    random.seed(random_seed)
     with open(join(base_path, f_name)) as read_file:
         data = json.load(read_file)
 
+    new_keys = list(data.keys())
+    random.shuffle(new_keys)
     data_new = {}
-    for key in data:
+    for key in new_keys:
         data_new[key] = data[key]
         if len(data_new) >= elems:
             break
 
-    with open(f_name, "w") as f:
+    path = pathlib.Path(".").joinpath(f_name)
+    path.parent.mkdir(parents=True, exist_ok=True)
+    with open(path, "w") as f:
         json.dump(data_new, f)
 
 
 def lmdb_subset_from_jsons(
-    base_path: str, f_name: str, json_base_path: str, jsons: Collection[str]
+    base_path: str,
+    f_name: str,
+    json_base_path: str,
+    jsons: Sequence[str],
+    target_csv_names: Sequence[str],
 ):
+    assert len(jsons) == len(
+        target_csv_names
+    ), "target names have to be as many as jsons"
     # collect names of all S2 patches
     subset_elems = []
-    for json_f_name in jsons:
+    for i, json_f_name in enumerate(jsons):
         with open(join(json_base_path, json_f_name)) as read_file:
             data = json.load(read_file)
-            for k in data:
-                subset_elems += [data[k]["S2_name"]]
+            s2_names = [data[x]["S2_name"] for x in data]
+            subset_elems += s2_names
+        with open(join(json_base_path, target_csv_names[i]), mode="w") as f:
+            writer = csv.writer(f)
+            writer.writerows([[x] for x in s2_names])
 
     data = {}
     # read data
@@ -59,18 +79,19 @@ def lmdb_subset_from_jsons(
 
 
 if __name__ == "__main__":
-    base_path_json = "/home/lhackel/Documents/datasets/BEN/VQA_RSVQAxBEN"
-    copy_part_of_json(base_path=base_path_json, f_name="RSVQAxBEN_QA_train.json")
-    copy_part_of_json(base_path=base_path_json, f_name="RSVQAxBEN_QA_val.json")
-    copy_part_of_json(base_path=base_path_json, f_name="RSVQAxBEN_QA_test.json")
+    base_path = "/home/lhackel/Documents/datasets/BEN/"
+    rsvqaxben_jsons = [
+        "VQA_RSVQAxBEN/RSVQAxBEN_QA_train.json",
+        "VQA_RSVQAxBEN/RSVQAxBEN_QA_val.json",
+        "VQA_RSVQAxBEN/RSVQAxBEN_QA_test.json",
+    ]
+    for f in rsvqaxben_jsons:
+        copy_part_of_json(base_path=base_path, f_name=f)
 
     lmdb_subset_from_jsons(
-        base_path="/home/lhackel/Documents/datasets/BEN/",
+        base_path=base_path,
         f_name="BigEarthNetEncoded.lmdb",
-        json_base_path="",
-        jsons=[
-            "RSVQAxBEN_QA_train.json",
-            "RSVQAxBEN_QA_val.json",
-            "RSVQAxBEN_QA_test.json",
-        ],
+        json_base_path="./",
+        jsons=rsvqaxben_jsons,
+        target_csv_names=["train.csv", "val.csv", "test.csv"],
     )

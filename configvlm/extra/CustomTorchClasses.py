8d4b2c2d1e5d202fca0dbc338b71f3e2317538d4
Leonard Hackel <l.hackel@tu-berlin.de>
2023-02-02 09:21:30 +0100

    adding tests for TorchCustomClasses and testing everything again for CI pipeline

--- configvlm/extra/CustomTorchClasses.py
+++ configvlm/extra/CustomTorchClasses.py
@@ -8,8 +8,13 @@ import torchvision.transforms.functional as TF
 class MyRotateTransform:
     # Rotates an image by one of the given degrees,
     # instead of a degree in the given range as transforms.RandomRotate does
+    # Rotation is counter-clockwise.
     # Code credit to https://github.com/pytorch/vision/issues/566
     def __init__(self, angles: Sequence[int]):
+        assert len(angles) > 0, (
+            "Choice of no angles not possible. "
+            "Please provide a sequence of at least length 1"
+        )
         self.angles = angles
 
     def __call__(self, x):
---
+++ tests/extra/test_CustomTorchClasses.py
@@ -0,0 +1,69 @@
+import pytest
+import torch
+
+from configvlm.extra.CustomTorchClasses import MyGaussianNoise
+from configvlm.extra.CustomTorchClasses import MyRotateTransform
+
+
+def test_rotate_single():
+    rt = MyRotateTransform([90])
+    t = torch.Tensor([[1, 0], [0, 2]]).unsqueeze(dim=0)
+    t = rt(t).squeeze()
+    assert torch.equal(t, torch.Tensor([[0, 2], [1, 0]]))
+
+
+def test_rotate_multiple():
+    rt = MyRotateTransform([0, 90, 180, 270])
+    t = torch.Tensor([[1, 0], [0, 2]]).unsqueeze(dim=0)
+    t = rt(t).squeeze()
+
+    valid_results = [
+        torch.Tensor([[1, 0], [0, 2]]),
+        torch.Tensor([[0, 1], [2, 0]]),
+        torch.Tensor([[2, 0], [0, 1]]),
+        torch.Tensor([[0, 2], [1, 0]]),
+    ]
+
+    res_arr = []
+    for v in valid_results:
+        res_arr.append(torch.equal(t, v))
+    assert sum(res_arr), (
+        f"Only exactly 1 rotation should be equal " f"but was {sum(res_arr)}"
+    )
+
+
+def test_empty_sequence():
+    with pytest.raises(AssertionError):
+        _ = MyRotateTransform([])
+
+
+def test_gaussian():
+    gt = MyGaussianNoise(sigma=1)
+    t = torch.Tensor([0])
+    assert t != gt(t), "No noise added"
+
+
+def test_gaussian_zero():
+    gt = MyGaussianNoise(sigma=0)
+    t = torch.Tensor([0])
+    assert t == gt(t), "No noise should be added"
+
+
+@pytest.mark.parametrize(
+    "dtype",
+    [
+        torch.float,
+        torch.double,
+        torch.half,
+        torch.bfloat16,
+        torch.complex32,
+        torch.complex128,
+        torch.uint8,
+        torch.int32,
+        torch.bool,
+    ],
+)
+def test_gaussian_dtype(dtype):
+    gt = MyGaussianNoise(sigma=0)
+    t = torch.ones([10, 10], dtype=dtype)
+    assert gt(t).dtype == t.dtype, "Datatype changed"

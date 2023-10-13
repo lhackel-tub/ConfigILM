6a5e06679a8bf98a06c9828092c61496f55c2693
Leonard Hackel <l.hackel@tu-berlin.de>
2023-10-13 16:45:13 +0200

    Adding Tests for print flags

--- tests/extra/test_data_RSVQAxBEN.py
+++ tests/extra/test_data_RSVQAxBEN.py
@@ -254,6 +254,12 @@ def test_dm_dataloaders(data_dir, bs: int):
     )
 
 
+@pytest.mark.parametrize("pi", [True, False])
+def test_dm_print_on_setup(data_dir, pi):
+    dm = RSVQAxBENDataModule(data_dir=data_dir, print_infos=pi)
+    dm.setup()
+
+
 def test_dm_shuffle_false(data_dir):
     dm = RSVQAxBENDataModule(data_dir=data_dir, shuffle=False)
     dm.setup(None)
